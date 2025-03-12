d30a4b9744a51c2060865e7c6e9e7ae8a57e3e21
Arpith Alexander <Arpith.Alexander@mobileye.com>
2025-03-12 16:39:18 +0100

    main.rs: Add random nickname, colors

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use tokio::io::{AsyncWriteExt, AsyncBufReadExt, BufReader};
 use tokio::sync::mpsc::{self, Sender, Receiver};
 use std::error::Error;
 use std::sync::Arc;
+use std::io::{Write, stdout};
+use rand::Rng;
+use colored::Colorize;
 
 #[derive(Clone)]
 struct IrcClient {
@@ -42,7 +45,7 @@ async fn init(client: Arc<IrcClient>) -> Result<(), Box<dyn Error + Send + Sync>
     client.sender.send(format!("CAP LS\r\n")).await?;
     client.sender.send(format!("NICK {}\r\n", client.nickname)).await?;
     client.sender.send(format!("USER {} {} {} :{}\r\n", client.username, client.nickname, client.server, client.realname)).await?;
-    client.sender.send(format!("JOIN {}\r\n", client.channel)).await?;
+    //client.sender.send(format!("JOIN {}\r\n", client.channel)).await?;
     Ok(())
 }
 
@@ -52,6 +55,16 @@ async fn write(client: Arc<IrcClient>, mut receiver: Receiver<String>) -> Result
         writer.write_all(format!("{}", message).as_bytes()).await?;
     }
     Ok(())
+}
+
+fn print_received_line(line: &str) {
+    let colon_index = line.find(" :").unwrap_or(0);
+    let prefix = &line[..colon_index];
+    let message = &line[colon_index..];
+    print!("{}", format!("RX: ").green());
+    print!("{}", format!("{}", prefix).yellow());
+    print!("{}", format!("{}", message).white());
+    stdout().flush().unwrap();
 }
 
 async fn read(client: Arc<IrcClient>) -> Result<(), Box<dyn Error + Send + Sync>> {
@@ -66,7 +79,7 @@ async fn read(client: Arc<IrcClient>) -> Result<(), Box<dyn Error + Send + Sync>
             break;
         }
         
-        println!("RECV: {}", line);
+        print_received_line(&line);
         if line.starts_with("PING") {
             let pong = line.replace("PING", "PONG");
             client.sender.send(pong).await?;
@@ -77,12 +90,17 @@ async fn read(client: Arc<IrcClient>) -> Result<(), Box<dyn Error + Send + Sync>
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let mut rng = rand::rng();
+
+    let mut random_nickname: String = String::from("bworm");
+    random_nickname.push_str(rng.random_range(0..=99999).to_string().as_str());
+
     let (client, receiver) = IrcClient::new(
         "irc.undernet.org", 
         "#bookz", 
-        "somedude42", 
-        "somedude42", 
-        "Some Dude"
+        &random_nickname, 
+        &random_nickname, 
+        "Book Worm"
     ).await?;
 
     let init_task = tokio::spawn(init(client.clone()));
